80ba9bbb18869317660f10f7dd6f7af82196aa5c
Egor <bamditse@gmail.com>
2022-04-18 20:35:12 +0700

    Updated Vernam cypher

--- cyphers/vernam_cypher.py
+++ cyphers/vernam_cypher.py
@@ -1,4 +1,4 @@
-import sys
+import bitarray
 
 
 class Vernam():
@@ -9,34 +9,17 @@ class Vernam():
         return self.__enc_dec(cyphertext, key)
 
     def __enc_dec(self, text, key):
-        if len(key) > len(text):
-            key = key[:len(text)]
-
-        try:
-            koi_text = text.encode('koi8-r', 'strict')
-        except:
-            print('Текст содержит недопустимые символы (КОИ-8)')
+        if text == '':
             return
 
-        try:
-            koi_key = key.encode('koi8-r', 'strict')
-        except:
-            print('Ключ содержит недопустимые символы (КОИ-8)')
+        if len(key) < len(text):
+            print('Короткий ключ')
             return
 
-        # if bytes('\r') in koi_text:
-        #     print('Ban')
-        print('Text:', koi_text)
-        print('Key:', koi_key)
+        text_bin = bitarray.bitarray()
+        text_bin.frombytes(text.encode('koi8-r'))
 
-        byte_text = int.from_bytes(koi_text, byteorder=sys.byteorder)
-        print('Byte text:', byte_text)
-        byte_key = int.from_bytes(koi_key, byteorder=sys.byteorder)
-        print('Byte key:', byte_key)
-        print('Encoded:', (byte_text ^ byte_key).to_bytes(len(text), byteorder=sys.byteorder))
-        print('Encoded:', (byte_text ^ byte_key))
-        enc_dec_text = (byte_text ^ byte_key).to_bytes(len(text), byteorder=sys.byteorder).decode('koi8-r')
-        print(enc_dec_text)
-        # print()
+        key_bin = bitarray.bitarray()
+        key_bin.frombytes(key[:len(text)].encode('koi8-r'))
 
-        return enc_dec_text
+        return (text_bin ^ key_bin).tobytes().decode('koi8-r')
